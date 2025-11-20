b43b299e174fa8384298fc5e7783da0286f3a358
Rogui20 <56708032+Rogui20@users.noreply.github.com>
2025-11-20 21:40:14 +0000

    Deploying to gh-pages from @ Rogui20/GTA5_Recast_Navmeshing@149d11b351911b396923b48443a76e7dd491a3a9 🚀

--- NavMeshBuild_8h.js
+++ NavMeshBuild_8h.js
@@ -3,5 +3,5 @@ var NavMeshBuild_8h =
     [ "NavmeshBuildInput", "structNavmeshBuildInput.html", "structNavmeshBuildInput" ],
     [ "BuildSingleNavMesh", "NavMeshBuild_8h.html#a5c31b2d6805d93b7ab4546a2ac413a6c", null ],
     [ "BuildSingleTile", "NavMeshBuild_8h.html#abef178e9026b2092c69014ad09006ab5", null ],
-    [ "BuildTiledNavMesh", "NavMeshBuild_8h.html#a073259c97407f6678086bfa867838b19", null ]
+    [ "BuildTiledNavMesh", "NavMeshBuild_8h.html#a4488738a7e3a925c4dab0e6f3803cbed", null ]
 ];
--- classNavMeshData.js
+++ classNavMeshData.js
@@ -1,7 +1,7 @@
 var classNavMeshData =
 [
     [ "~NavMeshData", "classNavMeshData.html#a5434728b0546753c3f01735e4a13e892", null ],
-    [ "BuildFromMesh", "classNavMeshData.html#a95f2c4f5cf74d4c3710acac8d6a4c246", null ],
+    [ "BuildFromMesh", "classNavMeshData.html#afcc7ca871eecfec2131711e8b32ac4de", null ],
     [ "BuildTileAt", "classNavMeshData.html#a309f93226ce1a4576d932144c9a70363", null ],
     [ "ExtractDebugMesh", "classNavMeshData.html#a1e3a68693ca1bb2655cc9db37a6f111f", null ],
     [ "IsLoaded", "classNavMeshData.html#ab3432212bbd65a1020926267863756fa", null ],
