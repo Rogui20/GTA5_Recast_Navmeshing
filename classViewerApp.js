90595453dbd977beb5294c76b39c06ff60c97219
Rogui20 <56708032+Rogui20@users.noreply.github.com>
2025-12-18 23:43:23 +0000

    Deploying to gh-pages from @ Rogui20/GTA5_Recast_Navmeshing@3f11dfc24f011bdaa80224ba86277da4e105fe8d 🚀

--- classViewerApp.js
+++ classViewerApp.js
@@ -2,8 +2,8 @@ var classViewerApp =
 [
     [ "ViewerApp", "classViewerApp.html#aae7f24aed89ead6de9de655db8b00757", null ],
     [ "~ViewerApp", "classViewerApp.html#a46f2542925089dbd0d8dc3742b458df6", null ],
-    [ "buildNavmeshDebugLines", "classViewerApp.html#a30b4ae10e356bfd02ba48f305e5e4e5c", null ],
-    [ "buildNavmeshFromMeshes", "classViewerApp.html#ae3d85da22e1bfac7ff7219b4410034ed", null ],
+    [ "buildNavmeshDebugLines", "classViewerApp.html#ad7854c8d28695ccf14cb9c4383dd3106", null ],
+    [ "buildNavmeshFromMeshes", "classViewerApp.html#ad0d77ac922b069812140752017ad898f", null ],
     [ "BuildStaticMap", "classViewerApp.html#adedb16a94f7e1707d83c258e2d009016", null ],
     [ "Init", "classViewerApp.html#adf79f3b7e38626f1b21ca54b3194e1d5", null ],
     [ "Run", "classViewerApp.html#ac32b6821de5456c23b16f33b6e8b87e0", null ],
