9415814701f8ef50ad6bb763fe421c0588da8e4b
Rogui20 <56708032+Rogui20@users.noreply.github.com>
2026-04-28 08:07:38 +0000

    Deploying to gh-pages from @ Rogui20/GTA5_Recast_Navmeshing@3c3a945a5c211c01840b9fe26fb66946226f1ad3 🚀

--- search/all_1b.js
+++ search/all_1b.js
@@ -11,12 +11,13 @@ var searchData=
   ['queryitems_8',['queryItems',['../classdtProximityGrid.html#a4ee1269c36500bb20d3891a2363c5ea8',1,'dtProximityGrid']]],
   ['queryname_9',['queryname',['../SDL__opengles2__gl2ext_8h.html#a97b96770d87ff5e95a4078847954c019',1,'queryName:&#160;SDL_opengles2_gl2ext.h'],['../SDL__opengl__glext_8h.html#a97b96770d87ff5e95a4078847954c019',1,'queryName:&#160;SDL_opengl_glext.h']]],
   ['querynamelength_10',['querynamelength',['../SDL__opengles2__gl2ext_8h.html#af02469a534b0deb873b6522a6d93a1b8',1,'queryNameLength:&#160;SDL_opengles2_gl2ext.h'],['../SDL__opengl__glext_8h.html#af02469a534b0deb873b6522a6d93a1b8',1,'queryNameLength:&#160;SDL_opengl_glext.h']]],
-  ['querypolygons_11',['querypolygons',['../classdtNavMeshQuery.html#a06e46d57afad3f07380d82bb8d4a6720',1,'dtNavMeshQuery::queryPolygons(const float *center, const float *halfExtents, const dtQueryFilter *filter, dtPolyRef *polys, int *polyCount, const int maxPolys) const'],['../classdtNavMeshQuery.html#aed2fc9071ca182a548baf5158cd4f441',1,'dtNavMeshQuery::queryPolygons(const float *center, const float *halfExtents, const dtQueryFilter *filter, dtPolyQuery *query) const']]],
+  ['querypolygons_11',['querypolygons',['../classdtNavMeshQuery.html#aed2fc9071ca182a548baf5158cd4f441',1,'dtNavMeshQuery::queryPolygons(const float *center, const float *halfExtents, const dtQueryFilter *filter, dtPolyQuery *query) const'],['../classdtNavMeshQuery.html#a06e46d57afad3f07380d82bb8d4a6720',1,'dtNavMeshQuery::queryPolygons(const float *center, const float *halfExtents, const dtQueryFilter *filter, dtPolyRef *polys, int *polyCount, const int maxPolys) const']]],
   ['querysuccess_12',['QuerySuccess',['../structImGuiStackLevelInfo.html#af73a14a915d748f1ebe5c4a4aedd5325',1,'ImGuiStackLevelInfo']]],
   ['querytiles_13',['queryTiles',['../classdtTileCache.html#afa3fb52e3082723c52a2048698a26cb3',1,'dtTileCache']]],
   ['question_20or_20problem_14',['Have a Question or Problem?',['../md_CONTRIBUTING.html#have-a-question-or-problem',1,'']]],
   ['questions_15',['Questions?',['../md_external_2SDL2_2docs_2README-emscripten.html#questions',1,'']]],
   ['queue_16',['Queue',['../structImGui__ImplVulkan__InitInfo.html#a43144f43d4eeb25a5230a55a1cea2bd4',1,'ImGui_ImplVulkan_InitInfo']]],
   ['queuefamily_17',['QueueFamily',['../structImGui__ImplVulkan__InitInfo.html#a51c934190a99b91180bf95ce56a36407',1,'ImGui_ImplVulkan_InitInfo']]],
-  ['quit_18',['quit',['../unionSDL__Event.html#a102a3008afe67a1c02ae7504e232dcef',1,'SDL_Event']]]
+  ['queueworldgeometry_18',['queueworldgeometry',['../ExternC_8cpp.html#acc04c41d3bdcc8c455199aab378060e8',1,'QueueWorldGeometry(void *navMesh, const char *pathToGeometry, Vector3 pos, Vector3 rot, const char *customID, bool preferBIN):&#160;ExternC.cpp'],['../ExternC_8h.html#acc04c41d3bdcc8c455199aab378060e8',1,'QueueWorldGeometry(void *navMesh, const char *pathToGeometry, Vector3 pos, Vector3 rot, const char *customID, bool preferBIN):&#160;ExternC.cpp']]],
+  ['quit_19',['quit',['../unionSDL__Event.html#a102a3008afe67a1c02ae7504e232dcef',1,'SDL_Event']]]
 ];
